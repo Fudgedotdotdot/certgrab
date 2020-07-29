ced99beb5958ed5ab54f9cc2ecdbdb1ce0ccce7e
Fudgedotdotdot <bussardvincent@gmail.com>
2020-07-29 09:32:00 +0000

    added source

---
+++ certgrab.go
@@ -0,0 +1,83 @@
+package main
+
+
+import (
+	"crypto/tls"
+	"net"
+	"time"
+	"fmt"
+	"bufio"
+	"strings"
+	"os"
+	"sync"
+	"flag"
+)
+
+
+func serverCert(ip string, port string, domain string){
+	// setting the timeout
+	d := &net.Dialer{
+		Timeout: time.Duration(3) * time.Second,
+	}
+
+	conn, err := tls.DialWithDialer(d, "tcp", ip+":"+port, &tls.Config{
+			InsecureSkipVerify: true,
+	})
+	if err != nil {
+		//fmt.Println(err)
+		return
+	}
+	cert := conn.ConnectionState().PeerCertificates[0].DNSNames
+
+	conn.Close()
+
+	// print the CommonName of the cert
+	CommonName := conn.ConnectionState().PeerCertificates[0].Subject.CommonName
+	if strings.HasSuffix(CommonName, domain){
+			fmt.Println(strings.TrimLeft(CommonName, "*."), ip)
+	}
+
+	// print the Subject Alternate Names
+	for _, name := range(cert){
+		if strings.HasSuffix(name, domain){
+			fmt.Println(strings.TrimLeft(name, "*."), ip)
+		}
+	}
+	return
+}
+
+func main(){
+		// parsing flags
+		var domain string
+		flag.StringVar(&domain, "domain", "", "root domain - if not specified, will match everything")
+		flag.Parse()
+
+		ips := make(chan string)
+		var wg sync.WaitGroup
+
+		// spawning goroutines
+		for i := 0; i < 150; i++{
+			wg.Add(1)
+			go func(){
+				for ip_port := range ips{
+					ip := strings.Split(ip_port, ":")[0]
+					port := strings.Split(ip_port, ":")[1]
+					serverCert(ip, port, domain)
+				}
+				wg.Done()
+			}()
+		}
+
+		// scanning stdin and launching goroutines
+		sc := bufio.NewScanner(os.Stdin)
+		for sc.Scan() {
+				ports := []string{"443", "4438", "9000", "591", "2082", "2087", "2095", "2096", "3000", "8000", "8001", "8008", "8080", "8083", "8443", "8834", "8888"}
+
+			for _, port := range ports {
+				ips <- fmt.Sprintf("%s:%s", sc.Text(), port)
+			}
+		}
+		close(ips)
+		wg.Wait()
+}
+
